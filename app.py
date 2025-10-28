ac3326c28ac9e04f3a81015e3efba12b0b6430e2
RaulLeon01 <20243rd013@utez.edu.mx>
2025-10-27 22:44:31 -0600

    Configuración Inicial y Corrección de Errores
    Crear el archivo app.py inicial

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, jsonify, render_template_string, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Lista para almacenar los dispositivos
+dispositivos_registrados = []
+
+@app.route('/formulario')
+def formulario():
+    return render_template_string("""
+    <html>
+    <head><title>Agregar Dispositivo</title></head>
+    <body>
+        <h1>Nuevo Dispositivo</h1>
+       <form action="/agregar" method="GET">
+            SID: <input type="text" name="sid"><br>
+            Nombre: <input type="text" name="nombre"><br>
+            IP: <input type="text" name="ip"><br>
+            Protocolos (separados por coma): <input type="text" name="protocolos"><br>
+            Observaciones: <input type="text" name="observaciones"><br>
+            <input type="submit" value="Registrar dispositivo">
+        </form>
+    </body>
+    </html>
+    """)
+
+@app.route('/agregar', methods=['GET'])
+def agregar_dispositivo():
+    # CORRECCIÓN: Se cambió request.arjs por request.args 
+    data = {
+        "sid": request.args.get('sid'),
+        "nombre": request.args.get('nombre'),
+        "ip": request.args.get('ip'),
+        "protocolos": [p.strip() for p in request.args.get('protocolos', '').split(',')],
+        "observaciones": request.args.get('observaciones')
+    }
+    dispositivos_registrados.append(data)
+    return redirect(url_for('mostrar_dispositivos'))
+
+@app.route('/dispositivos')
+def mostrar_dispositivos():
+    html = ""
+    for d in dispositivos_registrados:
+        html += f"""
+        <div style="border:1px solid #ccc; padding:10px; margin:10px;">
+            <strong>{d['sid']}</strong><br>
+            <b>Nombre:</b> {d['nombre']}<br>
+            <b>IP:</b> {d['ip']}<br>
+            <b>Protocolos:</b> {', '.join(d['protocolos'])}<br>
+            <b>Observaciones:</b> {d['observaciones']}
+        </div>
+        """
+
+    return render_template_string(f"""
+    <html>
+    <head><title>Dispositivos de Red</title></head>
+    <body>
+        <h1>Lista de Dispositivos</h1>
+        <a href="/formulario">Agregar nuevo dispositivo</a>
+        {html}
+    </body>
+    </html>
+    """)
+
+if __name__ == '__main__':
+    app.run(debug=True)
